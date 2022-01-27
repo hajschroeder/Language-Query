7fd09376ed07a49a4a0882450010c1cc24de2daa
Hans Schroeder <hajschroeder@gmail.com>
2022-01-27 11:08:35 -0800

    include favicon in html, and attempt to show/hide result. The page is now buggy

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
   $("#quiz").submit(function(event){
     event.preventDefault();
-
+    
     let qOneAnswer=parseInt($("#question1").val());
     let qTwoAnswer=parseInt($("#question2").val());
     let qThreeAnswer=parseInt($("#question3").val());
     let qFourAnswer=parseInt($("#question4").val());
     let qFiveAnswer=parseInt($("#question5").val());
-
+    
     $("#question1").text(qOneAnswer);
     $("#question2").text(qTwoAnswer);
     $("#question3").text(qThreeAnswer);
     $("#question4").text(qFourAnswer);
-    $("#question4").text(qFiveAnswer);
-
+    $("#question5").text(qFiveAnswer);
+    
     let result= qOneAnswer+qTwoAnswer+qThreeAnswer+qFourAnswer+qFiveAnswer
-      if(result<=203 && result!=131 && result!=122 && result!=50 && result!=41 && result!=32){
-        $("#resultA").show();
-      } else if (result<=230 && result !=221){
-        $("#resultB").show();
-      }else {
-        $("#resultC").show();
-      }
+  if(result<=203 && result !=140 && result!=131 && result!=122 && result!=50 && result!=41 && result!=32){
+      $("#resultA").show();
+    } else if (result<=230 && result !=221){
+      $("#resultB").show();
+    }else {
+      $("#resultC").show();
+    }
+    (function(event){
+      event.preventDefault();
+      $(".clickable").click(function(){
+        $("#hide-results").fadeToggle();
+        $("#results").fadeToggle();
+      });
+  });
   });
 });
